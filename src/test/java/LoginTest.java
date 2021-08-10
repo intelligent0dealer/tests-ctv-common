644e1b67a5b38505ee6f5692ece8ced3af559750
Artyom Morozov <artyom.morozov@motorsport.com>
2021-08-10 18:57:20 +0500

    login test. data providers

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,13 +1,22 @@
+import data.User;
 import mstv.pages.base.IHomePage;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import java.util.Collections;
+import java.util.Iterator;
 
 import static mstv.pages.base.IHomePage.openHomePage;
 
 public class LoginTest extends AbstractTest {
+    @DataProvider
+    private Iterator<Object[]> provideUsers() {
+        return Collections.singletonList(new Object[]{new User("log", "pass")}).iterator();
+    }
 
-    @Test
-    void test() {
+    @Test(dataProvider = "provideUsers")
+    void test(User user) {
         IHomePage homePage = openHomePage(getConfiguration(), getProtocol());
-        homePage.openLoginPage().typeEmail("login").typePassword("password").submit();
+        homePage.openLoginPage().typeEmail(user.getEmail()).typePassword(user.getPassword()).submit();
     }
 }
